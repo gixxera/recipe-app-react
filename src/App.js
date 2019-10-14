433f21fa30325cbafe5251866c6161703e67905c
Milen <monioo@abv.bg>
2019-10-14 16:41:36 +0300

    Setup api call

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 import Form from './components/Form';
 
+const API_KEY = '3c2d838b88724ded2c6bf235e4eb0a56';
+
 class App extends Component {
+  state = {
+    recipes: []
+  }
+  getRecipe = async (e) => {
+    e.preventDefault();
+    const recipeName = e.target.elements.recipeName.value;
+
+    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=10`);
+
+    const data = await api_call.json();
+
+    this.setState({
+      recipes: data
+    });
+    console.log(this.state.recipes);
+  }
   render() {
     return (
       <div className="app">
         <header className="app-header">
           <h1 className="app-title">Recipe Search</h1>
         </header>
-        <Form />
+        <Form getRecipe={this.getRecipe} />
       </div>
     );
   }
